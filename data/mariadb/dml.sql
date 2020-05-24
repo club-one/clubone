7e4caf13385bd9cddff9fe590319099d7f965abb
venkatasankara <46245191+venkatasankara@users.noreply.github.com>
2020-05-23 22:20:59 -0400

    Update dml.sql

--- data/mariadb/dml.sql
+++ data/mariadb/dml.sql
@@ -1,6 +1,6 @@
 
 use scheduling;
 
-insert into EventStatus (status_id, status_code, status_description) values(1,'CNFRM',"Confirmed"),(1,'CNCL',"Cancelled"),(1,'SCHED',"Scheduled"),(1,'PNLTY',"Cancelled with penalty");
+insert into EventStatus (status_id, status_code, status_description) values(1,'CNFRM',"Confirmed"),(1,'CNCL',"Cancelled Without Charges"),(1,'SCHED',"Scheduled / Planned"),(1,'PNLTY',"Cancelled with Charges");
 
 select * from EventStatus;
